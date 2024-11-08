709d464d07c365420462335466addb557dd6422f
omar-ahsan <omarkhattak0@gmail.com>
2024-11-08 18:17:02 +0500

    Added ERC4626 and constructors for all contracts

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,5 +1,15 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.20;
 
-contract Vault {}
+import {ERC4626} from "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC4626.sol";
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {MockToken} from "./mock/ERC20Token.sol";
+
+contract Vault is ERC4626 {
+    constructor(MockToken asset) ERC4626(asset) ERC20(
+        string(abi.encodePacked("Vault ", asset.name())), 
+        string(abi.encodePacked("v", asset.symbol()))
+    ) {
+            
+    }
+}
